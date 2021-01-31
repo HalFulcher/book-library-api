b991af38f43401d336ff8d1092899ff7a242d6e2
Hal Fulcher <hallam.fulcher@gmail.com>
2021-01-31 18:22:53 +0000

    Server running

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const app = express();
+const readerControllers = require("./controllers/readers");
+
+app.use(express.json());
+
+app.post("/readers", readerControllers.create);
+
+app.get("/readers", readerControllers.list);
+
+app.patch("/readers/:id", readerControllers.update);
+
+app.delete("/readers/:id", readerControllers.deleteReader);
 
 module.exports = app;
