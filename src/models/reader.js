f46b67f1086a981b6ddd9ff5ef6ff20b8776c0dc
Hal Fulcher <hallam.fulcher@gmail.com>
2021-04-08 22:05:55 +0100

    Add reader models

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,9 +1,30 @@
 module.exports = (connection, DataTypes) => {
-    const schema = {
-      name: DataTypes.STRING,
-      email: DataTypes.STRING,
-    };
-  
-    const ReaderModel = connection.define("Reader", schema);
-    return ReaderModel;
+  const schema = {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 50],
+        notEmpty: true,
+      },
+    },
   };
+
+  const ReaderModel = connection.define("Reader", schema);
+  return ReaderModel;
+};
